3f60fe11b4aee32e8e4813b3898712b82b099dce
we1li <125406661+we1li@users.noreply.github.com>
2023-05-10 14:08:14 +0300

    Update идз.py

--- идз.py
+++ идз.py
@@ -1,9 +1,13 @@
-print("Введите числа")
-a = tuple(map(int, input().split()))
-for i, x in enumerate(a):
-    if a[i] != a[-1]:
-        if a[i] >= a[i+1]:
-            print("Номер элемента нарушающего упорядоченность", i+1)
-            break
-    else:
-        print("Кортеж упородочен по возрастанию")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    print("Введите числа")
+    a = tuple(map(int, input().split()))
+    for i, x in enumerate(a):
+        if a[i] != a[-1]:
+            if a[i] >= a[i+1]:
+                print("Номер элемента нарушающего упорядоченность", i+1)
+                break
+        else:
+            print("Кортеж упородочен по возрастанию")
